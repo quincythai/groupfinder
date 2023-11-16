f3ccb0c471a6e230becf27c66f36b88ee3cd6d4a
Quincy Thai <quincythai50@icloud.com>
2023-11-15 18:08:29 -0800

    Add chakraprovider to main

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -17,6 +17,9 @@ function App() {
           </ul>
         </nav>
 
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+
         <Switch>
           <Route path="/about">
             <About />
