c8167640dcbd9c305b7a62a7de836d3149a395da
Quincy Thai <quincythai50@icloud.com>
2023-11-15 18:00:41 -0800

    Add prettier and begin router

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
