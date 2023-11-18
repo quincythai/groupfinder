3f58ca819fd5d69b1a16a557a45013c829da6e53
Quincy Thai <quincythai50@icloud.com>
2023-11-17 18:10:35 -0800

    Complete navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-slate-800">
-      <div className="">heloo</div>
+    <nav className="flex justify-between items-center bg-slate-800 p-6">
+      <div className="text-white text-2xl">
+        <span className="font-bold">Group</span>Finder
+      </div>
       <div>
-        <ul className="flex">
+        <ul className="flex space-x-4">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className="text-white">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" className="text-white">
+              About
+            </Link>
           </li>
         </ul>
       </div>
