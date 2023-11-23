631cb86cff1d006b4b6123cec560ed057ba80e78
Quincy Thai <quincythai50@icloud.com>
2023-11-22 23:20:41 -0800

    Refine home page

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
 import { Box, Button, Heading, Text } from '@chakra-ui/react'
 import Logo from '../components/Logo'
 import { Image } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
-import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <Box
-      maxW='70%'
-      mx='auto'
-      my='auto'
       textAlign='center'
-      h='100vh' // Set the height of the box to 100% of the viewport height
+      h='60vh'
       display='flex'
       flexDirection='column'
-      justifyContent='center' // Center the content vertically
+      justifyContent='center'
+      alignItems='center'
     >
-      <Box display='flex' alignItems='center' justifyContent='space-between'>
-        <Box display='flex' flexDirection='column' maxWidth='60%'>
+      <Box
+        display='flex'
+        alignItems='center'
+        justifyContent='space-between'
+        gap='20%'
+      >
+        <Box display='flex' flexDirection='column'>
           <Heading mb='4' fontSize='80px'>
             <Logo />
           </Heading>
