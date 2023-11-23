2cb5f1e68648474716b239293e0bebf88affe280
Quincy Thai <quincythai50@icloud.com>
2023-11-22 22:09:35 -0800

    link view info to modal

--- vite-project/src/components/GroupCard.jsx
+++ vite-project/src/components/GroupCard.jsx
@@ -10,12 +10,18 @@ import {
   Divider,
 } from '@chakra-ui/react'
 
+import GroupCardModal from './GroupCardModal'
+import React from 'react'
+import { useDisclosure } from '@chakra-ui/react'
+
 // TODO:
 // Figure out how to truncate descriptions that are too long
 // Center image
 // Have all same image sizes
 
 const GroupCard = ({ image, heading, text, numPeopleNeeded }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <Card minW='xs' maxW='xs'>
       <CardBody>
@@ -40,9 +46,10 @@ const GroupCard = ({ image, heading, text, numPeopleNeeded }) => {
         <Text alignSelf='end' color='blue.600' fontSize='2xl'>
           {numPeopleNeeded}
         </Text>
-        <Button variant='solid' colorScheme='blue'>
+        <Button variant='solid' colorScheme='blue' onClick={onOpen}>
           View Info
         </Button>
+        <GroupCardModal isOpen={isOpen} onClose={onClose} />
       </CardFooter>
     </Card>
   )
---
+++ vite-project/src/components/GroupCardModal.jsx
@@ -0,0 +1,41 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from '@chakra-ui/react'
+
+import React from 'react'
+
+const GroupCardModal = ({ isOpen, onClose }) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            magnam, nam laboriosam illo natus itaque in sit asperiores
+            exercitationem veritatis placeat fugiat eius ipsum cupiditate
+            corporis. Minima expedita laudantium alias?
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default GroupCardModal
