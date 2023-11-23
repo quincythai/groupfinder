8d8877bba2c46b9ceb2a70e5d930ea077113bd32
Quincy Thai <quincythai50@icloud.com>
2023-11-23 11:22:57 -0800

    Begin FAQ

--- vite-project/src/pages/Faq.jsx
+++ vite-project/src/pages/Faq.jsx
@@ -1,3 +1,13 @@
-const Faq = () => {}
+import { Flex, Heading } from '@chakra-ui/react'
+import FaqAccordion from '../components/FaqAccordion'
+
+const Faq = () => {
+  return (
+    <Flex justifyContent='center' padding='20px'>
+      <Heading>Frequently Asked Questions</Heading>
+      <FaqAccordion />
+    </Flex>
+  )
+}
 
 export default Faq
